7f9d6532cc755fa38f0e3343066e6713ab3d7857
EgoRomanoff <egoromanoff@mail.ru>
2022-01-21 01:00:35 +0300

    fixed bugs and rebuild functions

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ const toDo = document.querySelector('.todo'),
       enterBtn = document.querySelector('#enterBtn'),
       toDoList = document.querySelector('.todo__list')
 
+const noTasksLabel = createNewElement('span', 'todo__no-tasks-label')
+noTasksLabel.insertAdjacentText('afterbegin', 'No tasks yet')
+
 let editItemBtns, deleteItemBtns,
     listLength = toDoList.children.length,
     editedItem = undefined
 
-const toDoListSpan = document.createElement('span')
-toDoListSpan.className = 'todo__info-span'
-toDoListSpan.insertAdjacentText('afterbegin', 'No tasks yet')
+toggleNoTasksLabel()
 
 toDo.addEventListener('click', e => {
   deleteItemBtns = Array.from(document.querySelectorAll('.button--delete'))
@@ -22,16 +23,26 @@ toDo.addEventListener('click', e => {
   if (e.target === cancelBtn) closeCreateForm()
   if (e.target === enterBtn) {
     if (editedItem) {
-      editItemValues()
+      editToDoItem()
     } else {
       createToDoItem()
     }
   }
+  if (editItemBtns.includes(e.target)) sendItemValues(e.target)
   if (deleteItemBtns.includes(e.target)) deleteToDoItem(e.target)
-  if (editItemBtns.includes(e.target)) editToDoItem(e.target)
+  if (e.target === clearAllBtn) clearAllItems()
 
 })
 // ===== FUNCTIONS =====
+
+function toggleNoTasksLabel() {
+
+  !toDoList.children.length ?
+    toDoList.append(noTasksLabel) :
+    noTasksLabel.remove()
+
+}
+
 function openCreateForm(taskName = '', taskDescription = '', taskDatetime = '') {
 
   createForm.classList.add('showed')
@@ -53,16 +64,13 @@ function clearInputs() {
 
 function createToDoItem() {
 
-  if (!checkEmptyInput(inputName)) return 0
+  if (checkEmptyInput(inputName)) return 0
 
   let tagsArr = ['li', 'div', 'input', 'label', 'div', 'button', 'button', 'div', 'div']
 
-  let classesArr = ['todo__item', 'checkbox', 'checkbox__input', 'checkbox__label', 'item__buttons', 'button button--edit', 'button button--delete', 'item__content', 'item__name']
+  let classesArr = ['todo__item hidden', 'checkbox', 'checkbox__input', 'checkbox__label', 'item__buttons', 'button button--edit', 'button button--delete', 'item__content', 'item__name']
 
   let idsArr = [`item-${listLength + 1}`, undefined, `check-${listLength + 1}`, undefined, undefined, `edit-${listLength + 1}`, `delete-${listLength + 1}`, undefined, undefined]
-  // let newToDoItem = createNewElement('li', 'todo__item', `item-${listLength + 1}`)
-  // let checkboxElem
-  // let buttonsElem
 
   if (inputDescription.value !== '') {
     tagsArr.push('div')
@@ -75,44 +83,62 @@ function createToDoItem() {
   }
 
   let elemsArr = []
-// TODO: ОСТАНОВИЛСЯ ЗДЕСЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
   for (let i = 0; i < tagsArr.length; i++) {
-    elemsArr.push(createNewElement(tagsArr[i], classesArr[i]))
+    elemsArr.push(createNewElement(tagsArr[i], classesArr[i], idsArr[i]))
   }
 
-  checkboxElem = elemsArr[0]
-  checkboxElem.append(elemsArr[1], elemsArr[2])
+  let newItemElem = elemsArr[0]
+  let checkboxElem = elemsArr[1]
+  let btnsElem = elemsArr[4]
+  let contentElem = elemsArr[7]
+  checkboxElem.append(elemsArr[2], elemsArr[3])
+  btnsElem.append(elemsArr[5], elemsArr[6])
 
-  buttonsElem = elemsArr[3]
-  buttonsElem.append(elemsArr[4], elemsArr[5])
+  let itemNameElem = elemsArr[8]
 
-  elemsArr[6].insertAdjacentText('afterbegin', inputName.value)
-  newToDoItem.append(checkboxElem, buttonsElem, elemsArr[6])
+  itemNameElem.insertAdjacentText('afterbegin', inputName.value)
+
+  contentElem.append(itemNameElem)
 
   if (classesArr.includes('item__description') && classesArr.includes('item__datetime')) {
-    elemsArr[7].insertAdjacentText('afterbegin', inputDescription.value)
-    elemsArr[8].insertAdjacentText('afterbegin', convertDatetime(inputDatetime.value))
-    newToDoItem.append(elemsArr[7], elemsArr[8])
+    let itemDescriptionElem = elemsArr[9]
+    let itemDatetimeElem = elemsArr[10]
+
+    itemDescriptionElem.insertAdjacentText('afterbegin', inputDescription.value)
+    itemDatetimeElem.insertAdjacentText('afterbegin', convertDatetime(inputDatetime.value))
+
+    contentElem.append(itemDescriptionElem, itemDatetimeElem)
+
   } else if (classesArr.includes('item__description')) {
-    elemsArr[7].insertAdjacentText('afterbegin', inputDescription.value)
-    newToDoItem.append(elemsArr[7])
+
+    let itemDescriptionElem = elemsArr[9]
+
+    itemDescriptionElem.insertAdjacentText('afterbegin', inputDescription.value)
+
+    contentElem.append(itemDescriptionElem)
+
   } else if (classesArr.includes('item__datetime')){
-    elemsArr[7].insertAdjacentText('afterbegin', convertDatetime(inputDatetime.value))
-    newToDoItem.append(elemsArr[7])
+
+    let itemDatetimeElem = elemsArr[9]
+
+    itemDatetimeElem.insertAdjacentText('afterbegin', convertDatetime(inputDatetime.value))
+
+    contentElem.append(itemDatetimeElem)
+
   }
 
-  let infoSpan = document.querySelector('.todo__info-span')
-  if (infoSpan) toDoList.children[0].remove()
+  newItemElem.append(checkboxElem, btnsElem, contentElem)
+  toDoList.append(newItemElem)
 
-  toDoList.append(newToDoItem)
-  clearInputs()
-  closeAddForm()
+  closeCreateForm()
+
   setTimeout(() => {
     toDoList.lastChild.classList.remove('hidden')
   }, 0)
 
-  listLength++
-    // console.log(deleteItemBtn.length);
+  listLength = toDoList.children.length
+  toggleNoTasksLabel()
 }
 
 function checkEmptyInput(targetInput) {
@@ -149,21 +175,13 @@ function createNewElement(elemTagName, elemClassName, elemID = undefined) {
   return createdElem
 }
 
-function checkListLength() {
-  if (toDoList.children.length === 0) {
-    toDoList.append(toDoListSpan)
-  }
-}
+function convertDatetime(datetimeValue) {
+  const datetimeFromInput = /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2})/
+  const datetimeFromItem = /(\d{2})\.(\d{2})\.(\d{4}) - (\d{2}):(\d{2})/
 
-function convertDatetime(curValue) {
-  const re = /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2})/
-  const re2 = /(\d{2})\.(\d{2})\.(\d{4}) - (\d{2}):(\d{2})/
-
-  if (re.test(curValue)) {
-    return curValue.replace(re, '$3.$2.$1 - $4:$5')
-  } else {
-    return curValue.replace(re2, '$3-$2-$1T$4:$5')
-  }
+  return datetimeFromInput.test(datetimeValue) ?
+    datetimeValue.replace(datetimeFromInput, '$3.$2.$1 - $4:$5') :
+    datetimeValue.replace(datetimeFromItem, '$3-$2-$1T$4:$5')
 }
 
 function deleteToDoItem(targetBtn) {
@@ -171,46 +189,107 @@ function deleteToDoItem(targetBtn) {
   let targetItem = targetBtn.parentNode.parentNode
   targetItem.style.transitionDelay = '0s'
   targetItem.classList.add('hidden')
+
   setTimeout(() => {
     targetItem.remove()
-    checkListLength()
+    toggleNoTasksLabel()
   }, 400)
 
 }
 
-function editToDoItem(targetBtn) {
-
-  let targetItem = targetBtn.parentNode.parentNode
-  let targetItemChildren = Array.from(targetItem.children)
+function sendItemValues(targetBtn) {
 
   let values = []
-  // openAddForm()
-
-  if (targetItemChildren[2]) values.push(targetItemChildren[2].innerHTML)
-  if (targetItemChildren[3]) values.push(targetItemChildren[3].innerHTML)
-  if (targetItemChildren[4]) values.push(convertDatetime(targetItemChildren[4].innerHTML))
+  let targetItem = targetBtn.parentNode.parentNode
+  let targetValues = Array.from(targetItem.querySelector('.item__content').children)
 
   editedItem = targetItem.id
-  openAddForm(values[0], values[1], values[2])
 
-  console.log(values)
+  if (targetValues[0]) values.push(targetValues[0].innerText)
+  if (targetValues[1]) values.push(targetValues[1].innerText)
+  if (targetValues[2]) values.push(convertDatetime(targetValues[2].innerText))
+
+  openCreateForm(values[0], values[1], values[2])
+
 }
 
-function editItemValues() {
-  let targetItemChildren = document.querySelector(`#${editedItem}`).children
+function editToDoItem() {
 
-  if (inputName.value !== '') {
-    targetItemChildren[2].innerText = inputName.value
-  }
-  if (inputDescription.value !== '') {
-    targetItemChildren[3].innerText = inputDescription.value
-  }
-  if (inputDatetime.value !== '') {
-    targetItemChildren[4].innerText = convertDatetime(inputDatetime.value)
+  let targetItem = document.querySelector(`#${editedItem}`)
+  let targetItemContent = targetItem.querySelector('.item__content')
+
+  let targetItemName = targetItemContent.querySelector('.item__name')
+  let targetItemDescription = targetItemContent.querySelector('.item__description')
+  let targetItemDatetime = targetItemContent.querySelector('.item__datetime')
+
+  if (checkEmptyInput(inputName)) return 0
+
+  targetItemName.innerText = inputName.value
+
+  if (!checkEmptyInput(inputDescription)) {
+
+    if (!targetItemDescription) {
+
+      let newItemDescription = createNewElement('div', 'item__description')
+      newItemDescription.insertAdjacentText('afterbegin', inputDescription.value)
+      targetItemContent.append(newItemDescription)
+
+    } else {
+
+      if (inputDescription.value !== targetItemDescription.innerText) {
+        targetItemDescription.insertAdjacentText('afterbegin', inputDescription.value)
+      }
+
+    }
+
+  } else {
+
+    if (targetItemDescription) targetItemDescription.remove()
+
   }
 
-  closeAddForm()
-  // console.log(targetItem.children);
+  if (!checkEmptyInput(inputDatetime)) {
+
+    if (!targetItemDatetime) {
+
+      let newItemDatetime = createNewElement('div', 'item__datetime')
+      newItemDatetime.insertAdjacentText('afterbegin', convertDatetime(inputDatetime.value))
+      targetItemContent.append(newItemDatetime)
+
+    } else {
+
+      if (convertDatetime(inputDatetime.value) !== targetItemDatetime.innerText) {
+        targetItemDatetime.insertAdjacentText('afterbegin', convertDatetime(inputDatetime.value))
+      }
+
+    }
+
+  } else {
+
+    if (targetItemDatetime) targetItemDatetime.remove()
+
+  }
+
+  closeCreateForm()
+  editedItem = undefined
+
 }
 
-checkListLength()
+function clearAllItems() {
+
+  let toDoListChildren = toDoList.children
+
+  for (let item of toDoListChildren) {
+
+    item.style.transitionDelay = '0s'
+    item.classList.add('hidden')
+
+    setTimeout(() => {
+      item.remove()
+      toggleNoTasksLabel()
+      listLength = 0
+    }, 400)
+
+  }
+
+}
